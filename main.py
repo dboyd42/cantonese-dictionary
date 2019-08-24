3e00a2e093cde76146a10aded26f620527a17ab8
dboyd42 <bhatmagician@gmail.com>
2019-08-23 23:17:38 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# Copyright 2019 David Boyd, all rights reserved
+# Program: Cantonese Dictionary
+# Description:
+#     Creates a Cantonese dictionary.
+# Date: 2019/08/23
+# Revised:
+#     <revision date>
+
+# list libraries used
+import bs4, requests, webbrowser
+
+# Declare global constants
+
+def main():
+
+'''
+                        BLUEPRINT
+
+# (1) GET USER-DEFINED FILE
+# (2) PROCESS WORDS ONLINE
+# (3) OUTPUT TO FILE
+'''
+
+'''
+                   (1) GET USER-DEFINED FILE
+
+# (1)
+# (2)
+# (3)
+# (4)
+# (5)
+# (6)
+# (7)
+'''
+
+
+'''
+                   (2) PROCESS WORDS ONLINE
+
+# (1)
+# (2)
+# (3)
+# (4)
+# (5)
+# (6)
+# (7)
+'''
+
+
+'''
+                   (3) OUTPUT TO FILE
+
+# (1)
+# (2)
+# (3)
+# (4)
+# (5)
+# (6)
+# (7)
+'''
+
+# Call the main function
+main()
