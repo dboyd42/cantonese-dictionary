134c117ee69beef9f1cfd1af2a63c2b5ca01f65b
dboyd42 <bhatmagician@gmail.com>
2019-08-23 23:50:19 -0500

    pseudocode: blueprint

--- main.py
+++ main.py
@@ -25,39 +25,35 @@ def main():
 '''
                    (1) GET USER-DEFINED FILE
 
-# (1)
-# (2)
-# (3)
-# (4)
-# (5)
-# (6)
-# (7)
+# (1) get filename.ext // string var
+# (2) open the file    // file_obj var
+# (3) read first record's description field // string orig_words[]
+# (4) strip the \n from the description
+# (5) read the next record
+# (7) close the file
 '''
 
 
 '''
                    (2) PROCESS WORDS ONLINE
 
-# (1)
-# (2)
-# (3)
-# (4)
-# (5)
-# (6)
-# (7)
+## LOOP THROUGH WORD LIST
+# (1) Request (get) webpage using word from user's file // response_obj
+# (2) Check status of webpage
+# (3) Transfer page into an parser object  // bs4_object
+# (4) Parse and retireve data // string elements_2D_list
+#       4.1) English-definition, Hanzi
+# (5) CALL # (3) OUTPUT TO FILE
 '''
 
 
 '''
                    (3) OUTPUT TO FILE
 
-# (1)
-# (2)
-# (3)
-# (4)
-# (5)
-# (6)
-# (7)
+# (1) Create a file for writing
+# (2) Write the data as a record into the list: original word(s)
+# (3) Write the data as a record into the list: parsed English-def, Hanzi
+# (4) Close the file
 '''
 
 # Call the main function
