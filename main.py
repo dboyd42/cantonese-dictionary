10d098c7149c6bbc5fc7c7f19bdbcdf5a2a64bb5
dboyd42 <bhatmagician@gmail.com>
2019-08-24 23:53:37 -0500

    status=working | added modularization

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 #     Creates a Cantonese dictionary.
 # Date: 2019/08/23
 # Revised:
-#     <revision date>
+#     2019/08/24
 
 # list libraries used
 import bs4, requests
@@ -13,14 +13,29 @@ import bs4, requests
 
 # Declare global constants
 
-#def main():
-'''
-                        BLUEPRINT
+def main():
 
-# (1) GET USER-DEFINED FILE
-# (2) PROCESS WORDS ONLINE
-# (3) OUTPUT TO FILE
-'''
+    # Declare vars
+    user_file = ''  # str filename
+    lines   = []  # str lines from file
+    t_def   = []  # str Terms' definition
+    t_hanzi = []  # str Terms' Hanzi
+    t_roman = []  # str Terms' romanization
+
+    #user_file = "sample.txt"
+    #user_file = "sample2.txt"
+    #user_file = "sample3.txt"
+
+    # Get user-defined file
+    user_file = input('Enter the filename.ext: ')  # Uncomment line @ end of prj
+    lines = get_file(user_file)
+
+    # Process words online
+    t_def, t_hanzi, t_roman = process_lines(lines)
+
+    # Output to file
+    write_file(lines, t_def, t_hanzi, t_roman)
+
 
 '''
                    (1) GET USER-DEFINED FILE
@@ -31,47 +46,66 @@ import bs4, requests
 # (4) strip the '\n' && ',' from the description
 # (5) close the file
 '''
+
 # if argv -> determine '.' file_obj or word list sep by ','
 # if argv == 0 THEN
-print('\n\n')
-choice_word = input('Enter <filename.ext> or word1, word2: ')
-print('\n\n')
+#print('\n\n')
+#choice_word = input('Enter <filename.ext> or word1, word2: ')
+#print('\n\n')
 #choice_word = "zao an"   # Test word
 # create for loop to enter multiple words sep by ','
-lines = []
-lines.append(choice_word)
+#lines = []
+#lines.append(choice_word)
 # remove ',' delimeter
 #lines[0] = lines[0].rstrip(',')
-
-
-
-# then get file and yada yada yada
-# if ('.' is in choice_word): 
 # (1) Get the filename from user
-#user_file = input('Enter the filename.ext: ')  # Uncomment line @ end of prj
 #user_file = 'sample.txt'                       # Del line @ end of project
 #user_file = 'sample2.txt'                      # del after project completion
+# then get file and yada yada yada
+# if ('.' is in choice_word):
 
-#
-## (2) Open the file for reading
-#infile = open(user_file, 'r')
-#
-## (3) Read the first record's description field
-#lines = infile.readlines()
 
-# (4) strip the ',' and '\n' from the description
-nLines = len(lines)
-#for i in range(nLines):
-#    lines[i] = lines[i].rstrip('\n')
-#    if (',' in lines[i]):
-#        lines[i] = lines[i].rstrip(',')
+# Function get_file()
+# Description: Reads/imports the user defined file
+# Calls:
+# Parameters: string filename
+# Returns: string lines
 
-#DEBUG(PRINT USER FILE) = PASS
-#for i in range(nLines):
-#    print(lines[i])
+def get_file(filename):
 
-# (5) close the file
-#infile.close()
+    # Declare local variables
+    infile = filename
+    lines = []
+
+    # Open the file
+    infile = open(infile, 'r')
+
+    # Read record description fields
+    lines = infile.readlines()
+
+    # Strip the ',' and '\n' from the description
+    nLines = len(lines)
+    for i in range(nLines):
+        # Check if line has data
+        #if (lines[i] == '\n'):
+        lines[i] = lines[i].rstrip('\n')
+        if (',' in lines[i]):
+            lines[i] = lines[i].rstrip(',')
+    # Else keep blank lines
+    # End if
+
+    #DEBUG(PRINT USER FILE) = PASS
+    #for i in range(nLines):
+        #print(lines[i])
+
+    # Close the file
+    infile.close()
+
+    # Return values
+    return lines
+
+# End Function
+
 
 '''
                    (2) PROCESS WORDS ONLINE
@@ -83,50 +117,78 @@ nLines = len(lines)
 #       4.1) English-definition, Hanzi
 '''
 
+# Function process_lines()
+# Description: Defines the words from lines[]
+# Calls:
+# Parameters: string terms[]
+# Returns:
 
-# (1) Request (get) web page
-res = []  # response objects
-soup = [] # soup objects
+def process_lines(terms):
 
-for i in range(nLines):
-    # Init query for web address
-    address = lines[i]
-    address = address.replace(" ", "+")
-    # Request the web page and store inside a response object
-    response = requests.get('http://cantonese.org/search.php?q=' + address)
+    # Declare local variables
+    res     = []  # object Response
+    soup    = []  # object Soup
+    nTerms  = len(terms)  # len of term str
+    nSoups  = 0   # length of Soup objects
+    t_def   = []  # str Terms' definition
+    t_hanzi = []  # str Terms' Hanzi
+    t_roman = []  # str Terms' romanization
 
-    # (2) Check status of webpage
-    if (response.ok):
+    # Request web pages
+    for i in range(nTerms):
+        # Init query for web address
+        address = terms[i]
+        address = address.replace(" ", "+")
+
+        # Request the web page and store inside a response object
+        #if (terms[i] != '\n'):
+        response = requests.get('http://cantonese.org/search.php?q=' + address)
+        # Else keep blank lines
+
+        # Check status of web page
+        # If web page loads successfully (prot: 200)
+        #if (response.ok): # Else (prot: 400) = Don't create bs4 object
+        response.raise_for_status()
+
         # Store response object inside a response list
         res.append(response)
 
-        # (3) Create a BeautifulSoup object in a bs4 list
-        soup.append(bs4.BeautifulSoup(res[i].text, features="html.parser"))
+        # Append a BeautifulSoup object in a bs4 list
+        bs4_obj = (bs4.BeautifulSoup(res[i].text, "html.parser"))
+        soup.append(bs4_obj)
 
-# End for
+    # End for
 
-# (4) Parse and retireve data
-lenRows = len(res)
-hanzi = []
-definition = []
-roman = []
 
-# Get parsed data and store them in elems[]
-for i in range(lenRows):
+    # Loop through soup objects
+    nSoups = len(soup)
 
-    # get and store hanzi, definition, romanization from web page
-    roman.append(soup[i].select('.resulthead strong'))
-    try:
-        definition.append(soup[i].select('.defnlist li')[0])
-    except IndexError:
+    for i in range(nSoups):
+
+        # Parse and store data
+        t_roman.append(soup[i].select('.resulthead strong'))
+
         try:
-            definition.append(soup[i].select('.resultbody')[0])
-        except:
-            definition.append('null')
-    #definition.append(soup[i].select('.defnlist li'))
-    hanzi.append(soup[i].select('.resulthead'))
+            t_def.append(soup[i].select('.defnlist li')[0])
 
-# End for
+        except IndexError:
+            try:
+                t_def.append(soup[i].select('.resultbody')[0])
+
+            except:
+                t_def.append(' ')
+
+        #definition.append(soup[i].select('.defnlist li'))
+        t_hanzi.append(soup[i].select('.resulthead'))
+
+    # End for
+
+    # Return values
+    return t_def, t_hanzi, t_roman
+
+# End Function
+
+
 
 '''
                    (3) OUTPUT TO FILE
@@ -138,57 +200,79 @@ for i in range(lenRows):
 # (4) Close the file
 '''
 
-# Display to terminal
-try:
-    rom = roman[i][0].getText()
-except:
-    rom = lines[i]
-try:
-    define = definition[i].getText()
-except:
-    #define = ' '
-    print('NOT FOUND: Line#', str(i), ': ', str(lines[i]), sep='')
-    define = definition[i][0]
-    #notFound_file.write('Line #' +  str(i) + ',' + lines[i] + '\n')
+# Function write_file()
+# Description: Writes parsed data to CSV outfile
+# Calls:
+# Parameters: str list lines,
+#             str list t_def,
+#             str list t_hanzi,
+#             str list t_roman
+# Returns: None
 
-print(rom, ', ', define, ', ', sep='', end='\n')
+def write_file(lines, t_def, t_hanzi, t_roman):
+
+    # Declare local variables
+    not_def_num = []  # int line number from list
+    not_defined = []  # str terms not defined
+    s_def   = ''  # str term's definition
+    s_hanzi = ''  # str term's hanzi
+    s_roman = ''  # str term's romanization
+    lenRows = len(t_roman)
+    lenNotD = 0   # length of not_defined
+
+    # Create files for writing
+    outfile = open('canto-definitions.csv', 'w')
+    #notFound_outfile = open('canto-not-defined.csv', 'w')
+
+    # Init parsed text from bs4 objects
+    for i in range(lenRows):
+        try:
+            s_roman = t_roman[i][0].getText()
+            #print(t_roman[i][0])  # Displays obj if not found
+        except:
+            s_roman = lines[i]
+        try:
+            s_def = t_def[i].getText()
+        except:
+            print('NOT FOUND: LINE#', str(i+1), ': ', lines[i], sep='')
+            s_def = t_def[i][0]
+
+            # Append not_defined word list
+            #not_def_num.append( str(i) )
+            #not_defined.append( lines[0] )
+
+    # Write the data to the outfile
+        #print(s_roman, ', ', s_def, ', ', sep='', end='\n')
+        outfile.write(s_roman + ',' + s_def + ',' + '\n')
+
+    # End for
+
+    # Display/Write to the not found outfile
+    #lenNotD = len(not_defined)
+    #for i in range (lenNotD):
+        #print(not_defined)
+        #notFound_outfile.write(not_def_num[i] + ',' + not_defined[i] + '\n')
+
+    # (3) Close the file
+    outfile.close()
+    #notFound_outfile.close()
 
 
 
-# (1) Create a file for writing
-#outfile = open('canto-definitions.csv', 'w')
-# notFound_file = open('canto-not-defined.csv', 'w')
-# not_defined = []
-
-# (2) Write the data to the outfile
-#for i in range(lenRows):
-    # BUG: Hanzi is outputting wrong characters-------------------------------!
-    #    : -> Tried on LibreOffic, gEdit, Vim (Linux Platform)----------------!
-    #    : :: Possible Cause: Encoding Issues---------------------------------!
-    #    : ::       ==> (Linux? bs4?)-----------------------------------------!
-    # (1): -> Uncomment and open with Excel, Notepad, Etc (Windows Platform)--!
-    # (2): -> bs4 selector options (encoding, etc)----------------------------!
-    # https://stackoverflow.com/questions/15056633/python-find-text-using-beautifulsoup-then-replace-in-original-soup-variable
-
-    #outfile.write(roman[i][0].getText() + ',' + definition[i].getText() + \
-                  #',' + '\n')
-                  #',' + hanzi[i][0].getText() + '\n')
-#    try:
-#        rom = roman[i][0].getText()
-#    except:
-#        rom = lines[i]
-#    try:
-#        define = definition[i].getText()
-#    except:
-#        define = ' '
-#        print('NOT FOUND: Line#', str(i), ': ', lines[i], sep='')
-#        notFound_file.write('Line #' +  str(i) + ',' + lines[i] + '\n')
-#
-#    outfile.write(rom + ',' + define + ',' + '\n')
+    # Display to Terminal
+#        try:
+#            s_roman = t_roman[i][0].getText()
+#        except:
+#            s_roman = lines[i]
+#        try:
+#            s_def = t_def[i].getText()
+#        except:
+#            s_def = ' '
+#            print('NOT FOUND: Line#', str(i), ': ', lines[i], sep='')
+#            notFound_file.write('Line #' +  str(i) + ',' + lines[i] + '\n')
 
 
-# (3) Close the file
-#outfile.close()
-#notFound_file.close()
+# End Function
+
 # Call the main function
-#main()
+main()
