3446492dffa188bb32076cc898056c582803e522
David Boyd <44727721+dboyd42@users.noreply.github.com>
2019-09-07 08:13:25 -0500

    cleaned code

--- main.py
+++ main.py
@@ -5,13 +5,11 @@
 #     Creates a Cantonese dictionary.
 # Date: 2019/08/23
 # Revised:
+#     2019/09/07
 #     2019/08/24
 
 # list libraries used
 import bs4, requests
-# import webbrowser     # not used yet
-
-# Declare global constants
 
 def main():
 
@@ -35,35 +33,8 @@ def main():
 
     # Output to file
     write_file(lines, t_def, t_hanzi, t_roman)
-
-
-'''
-                   (1) GET USER-DEFINED FILE
-
-# (1) get filename.ext // string var
-# (2) open the file    // file_obj var
-# (3) read first record's description field // string orig_words[]
-# (4) strip the '\n' && ',' from the description
-# (5) close the file
-'''
-
-# if argv -> determine '.' file_obj or word list sep by ','
-# if argv == 0 THEN
-#print('\n\n')
-#choice_word = input('Enter <filename.ext> or word1, word2: ')
-#print('\n\n')
-#choice_word = "zao an"   # Test word
-# create for loop to enter multiple words sep by ','
-#lines = []
-#lines.append(choice_word)
-# remove ',' delimeter
-#lines[0] = lines[0].rstrip(',')
-# (1) Get the filename from user
-#user_file = 'sample.txt'                       # Del line @ end of project
-#user_file = 'sample2.txt'                      # del after project completion
-# then get file and yada yada yada
-# if ('.' is in choice_word):
-
+    
+# End Function
 
 # Function get_file()
 # Description: Reads/imports the user defined file
@@ -87,16 +58,12 @@ def get_file(filename):
     nLines = len(lines)
     for i in range(nLines):
         # Check if line has data
-        #if (lines[i] == '\n'):
+        # remove newline character
         lines[i] = lines[i].rstrip('\n')
+        # remove commas
         if (',' in lines[i]):
             lines[i] = lines[i].rstrip(',')
-    # Else keep blank lines
-    # End if
-
-    #DEBUG(PRINT USER FILE) = PASS
-    #for i in range(nLines):
-        #print(lines[i])
+        # End if
 
     # Close the file
     infile.close()
@@ -105,17 +72,6 @@ def get_file(filename):
     return lines
 
 # End Function
-
-
-'''
-                   (2) PROCESS WORDS ONLINE
-
-# (1) Request (get) webpage using word from user's file // response_obj
-# (2) Check status of webpage
-# (3) Transfer page into an parser object  // bs4_object
-# (4) Parse and retireve data // string elements_2D_list
-#       4.1) English-definition, Hanzi
-'''
 
 # Function process_lines()
 # Description: Defines the words from lines[]
@@ -141,13 +97,9 @@ def process_lines(terms):
         address = address.replace(" ", "+")
 
         # Request the web page and store inside a response object
-        #if (terms[i] != '\n'):
         response = requests.get('http://cantonese.org/search.php?q=' + address)
-        # Else keep blank lines
 
         # Check status of web page
-        # If web page loads successfully (prot: 200)
-        #if (response.ok): # Else (prot: 400) = Don't create bs4 object
         response.raise_for_status()
 
         # Store response object inside a response list
@@ -158,7 +110,6 @@ def process_lines(terms):
         soup.append(bs4_obj)
 
     # End for
-
 
     # Loop through soup objects
     nSoups = len(soup)
@@ -178,7 +129,6 @@ def process_lines(terms):
             except:
                 t_def.append(' ')
 
-        #definition.append(soup[i].select('.defnlist li'))
         t_hanzi.append(soup[i].select('.resulthead'))
 
     # End for
@@ -187,18 +137,6 @@ def process_lines(terms):
     return t_def, t_hanzi, t_roman
 
 # End Function
-
-
-
-'''
-                   (3) OUTPUT TO FILE
-
-
-# (1) Create a file for writing
-# (2) Write the data as a record into the list: original word(s)
-# (3) Write the data as a record into the list: parsed English-def, Hanzi
-# (4) Close the file
-'''
 
 # Function write_file()
 # Description: Writes parsed data to CSV outfile
@@ -222,13 +160,11 @@ def write_file(lines, t_def, t_hanzi, t_roman):
 
     # Create files for writing
     outfile = open('canto-definitions.csv', 'w')
-    #notFound_outfile = open('canto-not-defined.csv', 'w')
 
     # Init parsed text from bs4 objects
     for i in range(lenRows):
         try:
             s_roman = t_roman[i][0].getText()
-            #print(t_roman[i][0])  # Displays obj if not found
         except:
             s_roman = lines[i]
         try:
@@ -237,40 +173,14 @@ def write_file(lines, t_def, t_hanzi, t_roman):
             print('NOT FOUND: LINE#', str(i+1), ': ', lines[i], sep='')
             s_def = t_def[i][0]
 
-            # Append not_defined word list
-            #not_def_num.append( str(i) )
-            #not_defined.append( lines[0] )
-
     # Write the data to the outfile
-        #print(s_roman, ', ', s_def, ', ', sep='', end='\n')
         outfile.write(s_roman + ',' + s_def + ',' + '\n')
 
     # End for
 
-    # Display/Write to the not found outfile
-    #lenNotD = len(not_defined)
-    #for i in range (lenNotD):
-        #print(not_defined)
-        #notFound_outfile.write(not_def_num[i] + ',' + not_defined[i] + '\n')
-
     # (3) Close the file
     outfile.close()
     #notFound_outfile.close()
-
-
-
-    # Display to Terminal
-#        try:
-#            s_roman = t_roman[i][0].getText()
-#        except:
-#            s_roman = lines[i]
-#        try:
-#            s_def = t_def[i].getText()
-#        except:
-#            s_def = ' '
-#            print('NOT FOUND: Line#', str(i), ': ', lines[i], sep='')
-#            notFound_file.write('Line #' +  str(i) + ',' + lines[i] + '\n')
-
 
 # End Function
 
